4058dfed33169b1768553f1e675e1c84bd35318a
Zamba Izo <ofuzak@gmail.com>
2023-06-20 16:58:46 +0300

    wallet connect tests

--- src/providers/connectors/walletconnect.ts
+++ src/providers/connectors/walletconnect.ts
@@ -1,16 +1,13 @@
-const ConnectToWalletConnect = (
-  WalletConnectProvider: any,
-  opts: any
-) => {
-  return new Promise(async (resolve, reject) => {
-    const provider = WalletConnectProvider.init(opts);
-    try {
-      await provider.connect();
-      resolve(provider);
-    } catch (e) {
-      reject(e);
-    }
-  });
+const ConnectToWalletConnect = (WalletConnectProvider: any, opts: any) => {
+	return new Promise(async (resolve, reject) => {
+		const provider = await WalletConnectProvider.init(opts);
+		try {
+			await provider.connect();
+			resolve(provider);
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 
 export default ConnectToWalletConnect;
